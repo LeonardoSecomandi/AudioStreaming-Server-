8ad998e18b029893bd9a5ade7f69fd902012c461
Zaroual23 <62284738+Zaroual23@users.noreply.github.com>
2022-01-26 09:40:09 +0100

    third

--- Client_EasySound/Client_EasySound/Data/AppDbContext.cs
+++ Client_EasySound/Client_EasySound/Data/AppDbContext.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Client_EasySound.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Client_EasySound.Data
 {
-    
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
 
+        }
+        public DbSet<client> eleclient { get; set; }
+
+    }
 }
---
+++ Client_EasySound/Client_EasySound/Models/client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Client_EasySound.Models
+{
+    public class client
+    {
+        [Key]
+        public string id { get; set; }
+
+        [Required]
+        public string utente { get; set; }
+
+    }
+}
