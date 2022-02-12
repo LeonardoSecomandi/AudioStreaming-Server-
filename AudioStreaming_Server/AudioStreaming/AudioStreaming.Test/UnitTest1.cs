200cd7bdbd5f64273956cf662488247f395bbc6b
DarioRota <rota.dario.studente@itispaleocapa.it>
2022-02-12 11:08:47 +0100

    Versione 5.0

--- AudioStreaming_Server/AudioStreaming/AudioStreaming.Test/UnitTest1.cs
+++ AudioStreaming_Server/AudioStreaming/AudioStreaming.Test/UnitTest1.cs
@@ -1,13 +1,18 @@
 using System;
 using Xunit;
+using AudioStreaming.API.Models;
+using AudioStreaming.API.Data;
 
 namespace AudioStreaming.Test
 {
     public class UnitTest1
     {
+        private ApplicationDbContext _context { get; set; }
+
         [Fact]
-        public void Test1()
+        public void AddCanzone()
         {
+            CanzoniService canzoniService = new CanzoniService();
 
         }
     }
