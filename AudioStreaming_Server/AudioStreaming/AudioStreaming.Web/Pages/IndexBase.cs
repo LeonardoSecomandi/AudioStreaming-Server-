fcb2e1c3cdf044729dc443120816113c2abc3b03
LeonardoSecomandi <62284402+LeonardoSecomandi@users.noreply.github.com>
2022-02-15 11:46:27 +0100

    AudioStreamingServer

---
+++ AudioStreaming_Server/AudioStreaming/AudioStreaming.Web/Pages/IndexBase.cs
@@ -0,0 +1,24 @@
+﻿using AudioStreaming.Models;
+using AudioStreaming.Web.Service;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioStreaming.Web.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject]
+        public ICanzoniService canzoniService { get; set; }
+
+        public IEnumerable<Canzone> EleCanzoni { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            EleCanzoni = await canzoniService.GetCanzoni();
+            return;
+        }
+    }
+}
