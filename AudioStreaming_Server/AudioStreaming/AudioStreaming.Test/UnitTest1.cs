8d9535a17e9821c7f123d559abb261ce7ce79027
DarioRota <rota.dario.studente@itispaleocapa.it>
2022-02-15 11:30:16 +0100

    Test

--- AudioStreaming_Server/AudioStreaming/AudioStreaming.Test/UnitTest1.cs
+++ AudioStreaming_Server/AudioStreaming/AudioStreaming.Test/UnitTest1.cs
@@ -2,6 +2,8 @@ using System;
 using Xunit;
 using AudioStreaming.API.Models;
 using AudioStreaming.API.Data;
+using AudioStreaming.Models;
+using System.Collections.Generic;
 
 namespace AudioStreaming.Test
 {
@@ -12,8 +14,42 @@ namespace AudioStreaming.Test
         [Fact]
         public void AddCanzone()
         {
-            CanzoniService canzoniService = new CanzoniService();
+            //Arrange
+            CanzoniService canzoniService = new CanzoniService(_context);
+            API.Models.DTOS.Requests.CreateCanzoneRequest canzone = new API.Models.DTOS.Requests.CreateCanzoneRequest();
+            canzone.SongTitle = "Canzone 1";
+            canzone.AlbumName = "Album 1";
+            canzone.IDUserUploader = 1;
+            canzone.Duration = 30;
 
+            //Act
+            var result = canzoniService.AddCanzone(canzone);
+
+            //Assert   
+            Assert.True(result.Result.Success);
+
+        }
+        [Fact]
+        public void GetCanzoni()
+        {
+            //Arrange
+
+            CanzoniService canzoniService = new CanzoniService(_context);
+            API.Models.DTOS.Requests.CreateCanzoneRequest canzone = new API.Models.DTOS.Requests.CreateCanzoneRequest();
+            canzone.SongTitle = "Canzone 1";
+            canzone.AlbumName = "Album 1";
+            canzone.IDUserUploader = 1;
+            canzone.Duration = 30;
+
+
+            //Act
+           canzoniService.AddCanzone(canzone).Wait();
+
+           var result =  canzoniService.GetCanzoni().Result.GetEnumerator() as List<CanzoniService>;
+
+
+            //Assert
+            Assert.True(result.Count.Equals(1));
         }
     }
 }
