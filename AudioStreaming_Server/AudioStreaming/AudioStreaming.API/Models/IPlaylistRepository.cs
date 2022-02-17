52b0a33cc678fe17530a7e314f47339d51cd0953
DarioRota <rota.dario.studente@itispaleocapa.it>
2022-02-17 14:19:46 +0100

    PlaylistRepo

---
+++ AudioStreaming_Server/AudioStreaming/AudioStreaming.API/Models/IPlaylistRepository.cs
@@ -0,0 +1,23 @@
+﻿using AudioStreaming.API.Models.DTOS.Requests;
+using AudioStreaming.API.Models.DTOS.Responses;
+using AudioStreaming.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AudioStreaming.API.Models
+{
+    public interface IPlaylistRepository
+    {
+        public Task<CreatePlaylistResponse> CreatePlaylist(CreatePlaylistRequest req);
+        public Task<IEnumerable<Playlist>> GetPlaylist();
+        public Task<GetUserPlaylistResponse> GetUserPlaylist(int IdUser);
+
+        public Task<DeletePlaylistResponse> DeletePlaylist(int idPlaylist);
+
+        public Task<AddCanzoneToPlaylistResponse> AddCanzoneToPlaylist(AddCanzoneToPlaylistRequest req);
+
+
+    }
+}
